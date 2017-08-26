c9a0d3328545a0205484b900d7dd2084ffb2206b
Kevin Brown <chevinbrown@gmail.com>
2017-08-26 10:37:41 -0500

    Bootstrap variables suck

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'pages/home' => 'high_voltage/pages#show', id: 'home'
+  get 'pages/map' => 'high_voltage/pages#show', id: 'map'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170826150103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id"
+    t.jsonb "properties"
+    t.point "coordinates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
