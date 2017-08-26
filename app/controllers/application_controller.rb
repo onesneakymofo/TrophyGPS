7e4e74f378cb6445cff1ee0ed0bf353119fc13f1
Chris Autwell <cautwell@gmail.com>
2017-08-26 12:49:21 -0500

    Sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
   def ensure_signup_complete
      # Ensure we don't go into an infinite loop
      return if action_name == 'finish_signup'
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module TrophyGPS
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+   config.to_prepare do
+     # Configure single controller layout
+     Devise::SessionsController.layout "sign_in"
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
