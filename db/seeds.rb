a3cc772dda9f5b10e3f7581896c0bd14c342bd70
Kevin Brown <chevinbrown@gmail.com>
2017-08-26 12:10:24 -0500

    Users have posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 User.destroy_all
+Post.destroy_all
 
 20.times do
   first_name = Faker::Name.first_name
@@ -7,8 +8,18 @@ User.destroy_all
   name = "#{first_name} #{last_name}"
   puts "Creating user #{name}"
 
-  User.create(
+  user = User.create(
     name: name,
-    email: "#{name}@email.com"
+    email: "#{name.split(' ').join('_')}@email.com",
+    password: 'password'
+  )
+
+  post = Post.create(
+    user: user,
+    description: Faker::RickAndMorty.quote,
+    coordinates: [
+      Faker::Number.normal(33.513164, 0.00001),
+      Faker::Number.normal(-86.807892, 3.5)
+    ]
   )
 end
