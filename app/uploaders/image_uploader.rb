adc3c718dd648c6510772ae8816d12f4417f5bed
David H. Wilkins <dwilkins@conecuh.com>
2017-08-26 16:09:43 -0500

    Adding image processing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,16 @@
+require "image_processing/mini_magick"
+
 class ImageUploader < Shrine
+  include ImageProcessing::MiniMagick
   # plugins and uploading logic
   Shrine.plugin :determine_mime_type
+  Shrine.plugin :direct_upload
+  Shrine.plugin :processing
+  Shrine.plugin :cached_attachment_data
+
+  process(:store) do |io, context|
+    # mmi = MiniMagick::Image.read(io)
+    # mmi.exif
+  end
+
 end
