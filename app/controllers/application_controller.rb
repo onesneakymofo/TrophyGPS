2cb6bfe259f4dbf9442082b17d8e3f715878ce57
Chris Autwell <cautwell@gmail.com>
2017-08-27 06:07:18 -0500

    Remove byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, unless: :devise_controller?
   def after_sign_in_path_for(resource)
-    byebug
     authenticated_root_path || request.env['omniauth.origin'] || root_path
   end
 end
